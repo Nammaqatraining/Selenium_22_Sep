61d475ecb7720acfa36d2f46a9416a0405d71ab7
VinayNCLK <vinayn348@gmail.com>
2025-10-06 08:35:23 +0530

    06/10

---
+++ src/day6/Prog2.java
@@ -0,0 +1,23 @@
+package day6;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Prog2 {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("file:///C:/Users/Admin/eclipse-workspace-java/SeleniumPractice/src/html/Page8.html");
+		
+		driver.findElement(By.cssSelector("input:nth-child(1)")).sendKeys("ABC");
+		
+		driver.findElement(By.cssSelector("html input:nth-child(2)")).sendKeys("ABC");
+		
+		Thread.sleep(5000);
+		//driver.close();
+	}
+
+}
